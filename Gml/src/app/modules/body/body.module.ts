9925b3c608973c6663436a02dd02b51ab68843c3
yls <shanyilun@gmail.com>
2022-03-06 01:36:17 +0100

    Carousel fixed 50%

--- Gml/src/app/modules/body/body.module.ts
+++ Gml/src/app/modules/body/body.module.ts
@@ -4,16 +4,21 @@ import { CommonModule } from '@angular/common';
 import { BodyRoutingModule } from './body-routing.module';
 import { MainComponent } from './main/main.component';
 import { GaleriaComponent } from './galeria/galeria.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    GaleriaComponent
+    GaleriaComponent,
+    
   ],
   imports: [
     CommonModule,
     BodyRoutingModule,
-  ]
+    NgbModule
+  ],
+  exports: [GaleriaComponent],
+  bootstrap: [GaleriaComponent]
 })
 export class BodyModule { }
--- Gml/src/app/modules/body/galeria/galeria.component.ts
+++ Gml/src/app/modules/body/galeria/galeria.component.ts
@@ -7,7 +7,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./galeria.component.scss']
 })
 export class GaleriaComponent implements OnInit {
-  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   constructor() { }
 
   ngOnInit(): void {
