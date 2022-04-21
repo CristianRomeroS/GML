8addca88fa0aeac537b08b92a84fea2a18e4d49a
yls <shanyilun@gmail.com>
2022-04-22 01:20:55 +0200

    GDPR consent

--- Gml/src/app/app.component.ts
+++ Gml/src/app/app.component.ts
@@ -8,6 +8,24 @@ declare  var jQuery:  any;
 export class AppComponent implements OnInit {
   
   ngOnInit(): void {
+    let cc = window as any;
+    cc.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#3c404d",
+          "text": "#d6d6d6"
+        },
+        "button": {
+          "background": "#8bed4f"
+        }
+      },
+      "theme": "classic",
+      "content": {
+        "message": "Estamos usando cookies para mejorar experiencia del usuario y obtener datos analíticos. Puedes saber más sobre cookies aqui:",
+        "dismiss": "Aceptar",
+        "link": "Más info"
+      }
+    });
   }
   title = 'Gml';
 }
